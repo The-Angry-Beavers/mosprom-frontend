d5bb7d5f755f9d16b68f814785b2181f21021a9d
flutegod <julynish@mail.ru>
2025-10-18 05:18:50 +0300

    feat: namespace modal

--- src/widgets/namespace-list/index.tsx
+++ src/widgets/namespace-list/index.tsx
@@ -1,12 +1,42 @@
-import type { NamespaceType } from '@/entity';
-import { NamespaceCard } from './ui/namespace-card';
-import { Link } from 'react-router-dom';
-import css from './namespaceList.module.scss';
+import type { NamespaceType } from "@/entity";
+import { NamespaceCard } from "./ui/namespace-card";
+import { Link } from "react-router-dom";
+import css from "./namespaceList.module.scss";
+import { Form, Input, Modal } from "antd";
+import { useState } from "react";
 type Props = {
   items: NamespaceType[];
 };
 
+const NamespaceModal = ({
+  open,
+  setOpen,
+  onSubmit,
+}: {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  onSubmit: () => void;
+}) => {
+  return (
+    <Modal
+      open={open}
+      onCancel={() => setOpen(false)}
+      onOk={onSubmit}
+      okText="Создать"
+      cancelText="Отмена"
+    >
+      <h2>Создание проекта</h2>
+      <Form>
+        <Form.Item label="Название проекта">
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
 export const NamespaceList = ({ items }: Props) => {
+  const [open, setOpen] = useState(false);
   return (
     <div className={css.namespaceList}>
       {items.map((item, index) => (
@@ -14,7 +44,12 @@ export const NamespaceList = ({ items }: Props) => {
           <NamespaceCard namespace={item} />
         </Link>
       ))}
-	   <NamespaceCard namespace={{ id: 0, name: 'Добавить проект' }} addCard />
+      <NamespaceCard
+        namespace={{ id: 0, name: "Добавить проект" }}
+        addCard
+        onClick={() => setOpen(true)}
+      />
+      <NamespaceModal onSubmit={() => setOpen(false)} open={open} setOpen={setOpen} />
     </div>
   );
 };
--- src/widgets/namespace-list/ui/namespace-card/index.tsx
+++ src/widgets/namespace-list/ui/namespace-card/index.tsx
@@ -5,11 +5,12 @@ import { FolderOutlined, PlusOutlined } from "@ant-design/icons";
 type Props = {
   namespace: NamespaceType;
   addCard?: boolean;
+  onClick?: () => void;
 };
 
-export const NamespaceCard = ({ namespace, addCard }: Props) => {
+export const NamespaceCard = ({ namespace, addCard, onClick }: Props) => {
   return (
-    <Card className={css.card}>
+    <Card className={css.card} onClick={onClick}>
       <Flex gap={5} align="center" justify="center">
         {addCard ? (
           <PlusOutlined className={css.icon} />
